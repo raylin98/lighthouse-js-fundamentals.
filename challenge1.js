bddf77698e394f7e32fe4263459bf171047f9386
Raymond Lin <raymond.lin.7690@gmail.com>
2023-02-27 11:46:54 -0800

    yes

---
+++ challenge1.js
@@ -0,0 +1,17 @@
+function loopyLighthouse(x) {
+  for (x = 100; x <= 200; x++) {
+    if (x % 3 === 0 && x % 4 !== 0) {
+      console.log('Loopy');
+    }
+    else if (x % 3 !== 0 && x % 4 === 0) {
+      console.log('Lighthouse');
+    }
+    else if (x % 3 === 0 && x % 4 === 0) {
+      console.log('LoopyLighthouse');
+    }
+    else {
+      console.log(x);
+    }
+  }
+} 
+loopyLighthouse(100);
